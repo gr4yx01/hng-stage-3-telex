6c004d8183436cf28d675f2c777b581cd510797d
gr4yx01 <hoyx0101@gmail.com>
2025-02-19 15:54:25 +0100

    change endpoint

--- src/index.ts
+++ src/index.ts
@@ -2,28 +2,26 @@ import express, { json, NextFunction, Request, Response } from 'express'
 import cors from 'cors'
 import crypto from 'crypto'
 
-const GITHUB_SECRET = 'Pyr_hornet0101'; // Replace with your secret
+// const GITHUB_SECRET = 'Pyr_hornet0101'; // Replace with your secret
 
-function verifyGitHubSignature(req: Request, res: Response, next: NextFunction) {
-  const signature = req.headers['x-hub-signature'];
-  const payload = JSON.stringify(req.body);
+// function verifyGitHubSignature(req: Request, res: Response, next: NextFunction) {
+//   const signature = req.headers['x-hub-signature'];
+//   const payload = JSON.stringify(req.body);
 
-  const hmac = crypto.createHmac('sha1', GITHUB_SECRET);
-  hmac.update(payload);
+//   const hmac = crypto.createHmac('sha1', GITHUB_SECRET);
+//   hmac.update(payload);
 
-  const expectedSignature = `sha1=${hmac.digest('hex')}`;
+//   const expectedSignature = `sha1=${hmac.digest('hex')}`;
 
-  if (signature !== expectedSignature) {
-    res.status(401).send('Invalid signature');
-    return;
-  }
+//   if (signature !== expectedSignature) {
+//     res.status(401).send('Invalid signature');
+//     return;
+//   }
 
-  next(); // Proceed to the next middleware or route handler
-}
+//   next(); // Proceed to the next middleware or route handler
+// }
 
 const app = express()
-
-
 
 app.use(json())
 
@@ -35,17 +33,15 @@ app.get('/', (req, res) => {
     })
 })
 
-app.use(verifyGitHubSignature);
-
-app.post('/github-webhook', (req, res) => {
+app.post('/starred', (req, res) => {
+    console.log(req.body)
     const event = req.headers['x-github-event'];
     const payload = req.body;
   
-    // Check if the event is a "starred" event
     if (event === 'star') {
       const repoName = payload.repository.name;
       const userName = payload.sender.login;
-  
+
       console.log(payload.sender);
 
       res.status(200).send('Notification sent!');
